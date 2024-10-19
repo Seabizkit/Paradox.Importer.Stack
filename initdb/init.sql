67f8bb9cee0033fe534f50a5f60395a74f6dd11f
calvin.nel <calvin.nel@dpogroup.com>
2024-10-19 17:01:45 +0200

    WIP

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS DevParadoxConfig;
-GRANT ALL PRIVILEGES ON DevParadoxConfig.* TO 'ParadoxPiUser'@'%';
+--CREATE DATABASE IF NOT EXISTS DevParadoxConfig;
+--GRANT ALL PRIVILEGES ON DevParadoxConfig.* TO 'ParadoxPiUser'@'%';
 GRANT CREATE ON *.* TO 'ParadoxPiUser'@'%';
 FLUSH PRIVILEGES;
